e59a9765944d7f8ab68b44bcbcc8edfa72a7eb0d
broeringlucas <lucas10santos15@gmail.com>
2025-04-28 04:17:33 -0300

    remove imports

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 // Card.js
 import React from "react";
-import { Link, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "../styles/Card.css";
 
 const Card = ({ data }) => {
